0a887ab53824994c7bf7c76a20bd20c7507545f9
David A York <medmatix@gmail.com>
2021-12-06 17:48:50 -0500

    clear commit

--- testcode.py
+++ testcode.py
@@ -9,33 +9,41 @@ Created on Sep 30, 2018
 
 @author: david
 '''
-from tkinter import *
-import pandas as pd 
-from pandastable import Table
+#===============================================================================
+# from tkinter import *
+# import pandas as pd 
+# from pandastable import Table
+# 
+# 
+# 
+# 
+# from tkinter import *
+# from pandastable import Table, TableModel
+#  
+# class TestApp(Frame):
+#     """Basic test frame for the table"""
+#     def __init__(self, parent=None):
+#         self.parent = parent
+#         Frame.__init__(self)
+#         self.main = self.master
+#         self.main.geometry('600x400+200+100')
+#         self.main.title('Table app')
+#         f = Frame(self.main)
+#         f.pack(fill=BOTH,expand=1)
+#         #df = TableModel.getSampleData()
+#         df = pd.read_csv('co2.csv')
+#         pt = Table(f, dataframe=df,
+#                                 showtoolbar=True, showstatusbar=True)
+#         pt.show()
+#         return
+#  
+# app = TestApp()
+# #launch the app
+# app.mainloop()
+#===============================================================================
 
-
-
-
-from tkinter import *
-from pandastable import Table, TableModel
- 
-class TestApp(Frame):
-    """Basic test frame for the table"""
-    def __init__(self, parent=None):
-        self.parent = parent
-        Frame.__init__(self)
-        self.main = self.master
-        self.main.geometry('600x400+200+100')
-        self.main.title('Table app')
-        f = Frame(self.main)
-        f.pack(fill=BOTH,expand=1)
-        #df = TableModel.getSampleData()
-        df = pd.read_csv('co2.csv')
-        pt = Table(f, dataframe=df,
-                                showtoolbar=True, showstatusbar=True)
-        pt.show()
-        return
- 
-app = TestApp()
-#launch the app
-app.mainloop()
+# Accessing an R object from from python 
+# (example from: http://rpy.sourceforge.net/rpy2/doc-dev/html/introduction.html)
+import rpy2.robjects as robjects
+rpi = robjects.r['pi']
+print("PI from R = ", rpi)
